7dd99e106359a4a2fc5e98ba9010a1512855dd9a
Anka <runner@Mac-1729842975625.local>
2024-10-25 08:08:39 +0000

    Update Package.swift for version 0.2.4

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "StandardComponents",
-            type: .dynamic,
             targets: ["StandardComponents"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "StandardComponents",
-            url: "https://github.com/EYDS-CA/standard-components-ios/releases/download/0.2.2/StandardComponents.xcframework.zip",
-            checksum: "c16ffdfe4abe2471bcee971fc29cb8788bf0fac7742672eb9a07771ef1b95a31"
+            url: "https://github.com/EYDS-CA/standard-components-ios/releases/download/0.2.4/StandardComponents.xcframework.zip",
+            checksum: "94b39eb6e03640d27029c863d53b9c025cf1f5b3b7666b67981144a98ed47b0d"
         )
     ]
 )
