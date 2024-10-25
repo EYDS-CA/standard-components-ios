181ee05cfb63a83efc57978021da035578ef0bc6
Anka <runner@Mac-1729864049310.local>
2024-10-25 13:58:31 +0000

    Update Package.swift for version 0.3.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "StandardComponents",
-            url: "https://github.com/EYDS-CA/standard-components-ios/releases/download/0.2.9/StandardComponents.xcframework.zip",
-            checksum: "d68bf55f5de13205d206b5d38a235d74f611190abac09f5553082ff25134e35e"
+            url: "https://github.com/EYDS-CA/standard-components-ios/releases/download/0.3.0/StandardComponents.xcframework.zip",
+            checksum: "a36d7cefd2980580210026b78cf070a7989122d078c1e4194c0f444a9a29e3cd"
         )
     ]
 )
